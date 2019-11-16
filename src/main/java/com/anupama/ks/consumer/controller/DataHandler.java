c961ea8d1cfd19eb35347cf68adc324bed9053e8
Anupama <akhangar@cisco.com>
2019-11-16 18:30:59 +0530

    Authenticte mac address of client requested device and then post the data

--- src/main/java/com/anupama/ks/consumer/controller/DataHandler.java
+++ src/main/java/com/anupama/ks/consumer/controller/DataHandler.java
@@ -11,26 +11,29 @@ import com.anupama.ks.consumer.service.FileHandler;
 
 @RestController
 public class DataHandler {
-	
-    @RequestMapping(value = "/data/consumer", method = RequestMethod.GET,
-    produces = MediaType.ALL_VALUE)
+
+	@RequestMapping(value = "/data/consumer", method = RequestMethod.GET,
+			produces = MediaType.ALL_VALUE)
 	public String consumeData(@RequestParam(value = "data", required = true) String dataPoint,
 			@RequestParam(value = "mac", required = true) String macAddress) {
-    	String data = "Fail to write data";
-    	FileHandler handler = new FileHandler();
-    	if(handler.isAuthentic(macAddress)) {
-    	handler.writeData(dataPoint);
-    	data= "Data written successfully";
-    	}
-    	System.out.println(data);
+		String data = "Fail to write data";
+		FileHandler handler = new FileHandler();
+		if(handler.isAuthentic(macAddress)) {
+			handler.writeData(dataPoint);
+			data= "Data written successfully";
+		}
+		System.out.println(data);
 		return data;
 	}
-    
-    @RequestMapping(value = "/data/producer", method = RequestMethod.POST,
-    produces = MediaType.APPLICATION_JSON_VALUE)
-	public DeviceTO produceData() {
-    	FileHandler handler = new FileHandler();
-    	DeviceTO deviceData = handler.readData();
+
+	@RequestMapping(value = "/data/producer", method = RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public DeviceTO produceData(@RequestParam(value = "mac", required = true) String macAddress) {
+		FileHandler handler = new FileHandler();
+		DeviceTO deviceData = null;
+		if(handler.isAuthentic(macAddress)) {
+			deviceData = handler.readData();
+		}
 		return deviceData;
 	}
 
