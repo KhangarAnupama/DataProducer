31f1a88eaf4066d055d82012cd3b1a80073eae48
Anupama <akhangar@cisco.com>
2019-11-16 16:45:51 +0530

    Service consume and produce data

---
+++ src/test/java/com/anupama/ks/consumer/DataConsumer/DataConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anupama.ks.consumer.DataConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
