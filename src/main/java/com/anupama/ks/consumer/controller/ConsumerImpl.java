19e1569812273f3887d9c514dde34462ce96f90b
Anupama <akhangar@cisco.com>
2019-11-18 00:00:36 +0530

    Service collects data from the simulator and sends it to Kafka pipeline

--- src/main/java/com/anupama/ks/consumer/controller/ConsumerImpl.java
+++ src/main/java/com/anupama/ks/consumer/controller/ConsumerImpl.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.anupama.ks.consumer.service.FileHandler;
-
 
 @RestController
 public class ConsumerImpl {
